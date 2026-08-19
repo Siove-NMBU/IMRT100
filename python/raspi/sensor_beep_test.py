a1e15d720b857b7a15c5f6d589b09474526594df
Cinders <siove1622@nmbu.no>
2026-08-19 15:17:03 +0200

    More code changes

---
+++ python/raspi/sensor_beep_test.py
@@ -0,0 +1,29 @@
+import imrt_robot_serial
+import RPi.GPIO as GPIO
+import time
+
+BUZZ_PIN = 23
+DUTY = 10
+
+# BCM pin naming
+GPIO.setmode(GPIO.BCM)
+
+# Turn off GPIO warnings
+GPIO.setwarnings(False)
+
+# Set buzzer pin to output
+GPIO.setup(BUZZ_PIN, GPIO.OUT)
+
+p = GPIO.PWM(BUZZ_PIN, 250)  # channel=12 frequency=50Hz
+p.start(0)
+
+# Test
+p.ChangeDutyCycle(DUTY)
+p.ChangeFrequency(440)
+time.sleep(2)
+p.ChangeDutyCycle(0)
+time.sleep(1)
+
+GPIO.cleanup()
+print("Goodbye")
+p.stop()
