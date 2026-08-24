cb620c4646f014bd8f0fdf3290c4aac13fafa21f
Kenrekt <hdlightzz@gmail.com>
2026-08-24 14:38:47 +0200

    Finjusteringer

--- python/raspi/soundtest.py
+++ python/raspi/soundtest.py
@@ -1,7 +1,9 @@
 from playsound import playsound
 import os
 
-path = "/home/pi/python/raspi/soundfiles/FAH.mp3"
+# path = "/home/pi/python/raspi/soundfiles/FAH.mp3"
+path = "/home/student/Desktop/Link to RoboCode/IMRT100/python/raspi/soundfiles/FAH.wav"
+
 
 print("File exists:", os.path.exists(path))
 print("Playing:", path)
