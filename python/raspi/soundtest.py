7c4d3484ea856b685f911f7305c1a5873cd714e6
Kenrekt <hdlightzz@gmail.com>
2026-08-24 14:44:19 +0200

    Finjusteringer

--- python/raspi/soundtest.py
+++ python/raspi/soundtest.py
@@ -1,19 +1,7 @@
-from playsound import playsound
 import pygame
 import time
 import os
 
-'''# path = "/home/pi/python/raspi/soundfiles/FAH.mp3"
-path = "/home/student/Desktop/Link to RoboCode/IMRT100/python/raspi/soundfiles/FAH.wav"
-
-
-print("File exists:", os.path.exists(path))
-print("Playing:", path)
-
-playsound(path)
-
-print("Finished")
-'''
 
 pygame.mixer.init()
 
