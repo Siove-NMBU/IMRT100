4c72ba330f15ed30627bcb37b4f12acc52d78a1d
Kenrekt <hdlightzz@gmail.com>
2026-08-24 14:19:22 +0200

    Finjusteringer

--- python/raspi/soundtest.py
+++ python/raspi/soundtest.py
@@ -2,11 +2,12 @@ from playsound import playsound
 # pip install playsound==1.2.2
 import threading
 
+path = "/home/pi/python/raspi/soundfiles/FAH.mp3" 
 
 def play(path):
     playsound(path)
 
 
 # t = threading.Thread(target=play, args=("C://Users//sindr//Music//Rnd Samples//terrible_drink_II.wav",))
-t = threading.Thread(target=play, args=("python/raspi/soundfiles/FAH.mp3",))
+t = threading.Thread(target=play, args=(path),)
 t.start()
