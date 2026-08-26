3fbfd9dbf24ae1223722c5db3223539e39ff1e8f
Cinders <siove1622@nmbu.no>
2026-08-26 10:30:47 +0200

    Cleaned up and worked on robocode

--- python/raspi/G6_robocode.py
+++ python/raspi/G6_robocode.py
@@ -138,7 +138,7 @@ while not motor_serial.shutdown_now:
 
     # Calculate motor mix differentials for the iteration
     diff += 0 if (dTL > 0) else -int(abs(dTL)**E_POW)
-    diff += -0.6 * dist_right if (dTR > 0) else int(abs(dTR)**E_POW)
+    diff += -0.8 * dist_right if (dTR > 0) else int(abs(dTR)**E_POW)
     # diff += round(-DRIFT_BIAS * dist_right)  # Krenging til høyre
 
     # Motor mix
