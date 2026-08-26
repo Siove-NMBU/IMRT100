429cfb2d3c69b743b68662251708d96973c5df00
Cinders <siove1622@nmbu.no>
2026-08-26 10:12:12 +0200

    Cleaned up and worked on robocode

--- python/raspi/G6_robocode.py
+++ python/raspi/G6_robocode.py
@@ -84,7 +84,7 @@ for soundpaths in list_sounds:
 
 # Boot sound
 boot_sound = pygame.mixer.Sound(
-    "/home/student/Desktop/Link to RoboCode/IMRT100/python/raspi/soundfiles/bring-it-on-siege.mp3"
+    "/home/student/Desktop/Link to RoboCode/IMRT100/python/raspi/soundfiles/bring-it-on-seige.wav"
 )
 if not pygame.mixer.get_busy():
     boot_sound.play()
